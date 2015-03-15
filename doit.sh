1ac10c5c897cb8843a085a5688a0c390bdc7c42b
Fredrik Wendt <fredrik@wendt.se>
2015-03-15 01:52:59 +0100

    fixed counter issue which was lost somehow

--- doit.sh
+++ doit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 for i in ubuntu:latest dockerfile/nginx:latest orchardup/php5:latest orchardup/mysql:latest mysql:latest busybox:latest ; do
+    COUNT=`date +%s`
     TAG=registry.dockerclass.com/dummy/image_$COUNT
     cat > Dockerfile <<EOF
 FROM $i
@@ -8,4 +9,5 @@ RUN touch /new_file
 EOF
     docker build -t $TAG .
     docker push $TAG
+    sleep 1
 done
